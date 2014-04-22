bfd396370a6a934d5a21ad86aae2534c19d8812a
Juliette TRAN <applejuice013@hotmail.fr>
2014-04-22 20:33:25 +0800

    update launcher

---
+++ ShootEmUpLauncher/ShootEmUpLauncher/src/Core.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SFML;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace ShootEmUpLauncher
+{
+    class Core
+    {
+        private string      _name = "abc";
+        private string      _author = "auteur";
+        private string      _description = "blablabla";
+        private int         _orientation = 0;
+        private UserShip    _player;
+        private List<Level> _levels;
+
+        static void Main()
+        {
+            // Récupérer données du jeu
+            Core game = new Core();
+            // Set X et Y de la fenêtre (ici random..)
+            uint x = 600;
+            uint y = 800;
+
+            if (game._orientation == 0) // 0 = Vertical
+            {
+                x = 500;
+                y = 800;
+            }
+
+            SFML.Window.Window window = new SFML.Window.Window(new VideoMode(y, x), game._name);
+
+        }
+    }
+}
---
+++ ShootEmUpLauncher/ShootEmUpLauncher/src/EnemyShip.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShootEmUpLauncher
+{
+    class EnemyShip
+    {
+        int _point { get; set; }
+        string _shipSprite { get; set; }
+        string _weaponSprite { get; set; }
+        int _damage { get; set; }
+        int _fireRate { get; set; }
+    }
+}
