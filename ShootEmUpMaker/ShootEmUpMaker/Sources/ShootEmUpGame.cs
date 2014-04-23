d90da174d5ecd29ef8f12020fa69a830baefd489
Steeve Pommier <pommier.steeve@gmail.com>
2014-04-23 06:43:17 +0200

    maker modified

---
+++ ShootEmUpMaker/ShootEmUpMaker/Sources/ShootEmUpGame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShootEmUpMaker
+{
+    class ShootEmUpGame
+    {
+        string _gameName { get; set; }
+        string _author { get; set; }
+        string _descrption { get; set; }
+        enum _orientation
+        {
+            Horizontal,
+            Vertical
+        };
+    }
+}
