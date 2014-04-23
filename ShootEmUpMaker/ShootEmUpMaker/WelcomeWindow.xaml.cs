3945461cc4e0d189de5794c58b34417d3a6b6b22
Dagasan <cagri.dagasan@gmail.com>
2014-04-23 18:37:52 +0800

    grid DONE

--- ShootEmUpMaker/ShootEmUpMaker/WelcomeWindow.xaml.cs
+++ ShootEmUpMaker/ShootEmUpMaker/WelcomeWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace ShootEmUpMaker
 {
@@ -22,6 +23,51 @@ namespace ShootEmUpMaker
         public WelcomeWindow()
         {
             InitializeComponent();
-        }
+
+            try
+            {
+                int i = 1;
+                string Docpath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                string finalpath = Docpath + "\\" + "ShootEmUpMaker";
+                string[] filePaths = Directory.GetFiles(@finalpath, "*.xml");
+                foreach (string x in filePaths)
+                {
+                    var border = new Border();
+                    var text = new TextBlock();
+                    text.HorizontalAlignment = HorizontalAlignment.Center;
+                    text.VerticalAlignment = VerticalAlignment.Center;
+                    text.FontSize = 36;
+                    text.Foreground = new SolidColorBrush(Color.FromRgb(255,255,255));
+                    //text.FontWeight = FontWeight;
+                    text.Text = x;
+                    //<TextBlock x:Name="CreateButton" Text="+" HorizontalAlignment="Center"
+                    //VerticalAlignment="Center" ToolTip="Click here to create a new game" FontSize="36" Foreground="White" FontWeight="Thin" />
+
+                    border.Child = text;
+                    border.Name = "Game"  + i.ToString();
+                    border.Width = 140;
+                    border.Height = 260;
+                    border.Background = new SolidColorBrush(Color.FromRgb(0, 111, 111));
+                    border.Margin = new Thickness(5,5,5,5);
+
+                    this.Games.Children.Add(border);
+                    i++;
+                    MessageBox.Show(x);
+                }
+            }
+            
+            catch (Exception e)
+            {
+                Console.WriteLine("The process failed: {0}", e.ToString());
+            }
+
+            
+
+           
+
+           // var toto = new Label();
+            //toto.Content = "toto";
+            //this.Games.Children.Add(toto);
+        }    
     }
 }
