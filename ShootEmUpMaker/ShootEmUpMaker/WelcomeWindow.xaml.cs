6676e22023afa414a24e45e1800a6ad1992224db
Steeve Pommier <pommier.steeve@gmail.com>
2014-04-24 06:38:31 +0200

    Wecolme board changed
    new game color card

--- ShootEmUpMaker/ShootEmUpMaker/WelcomeWindow.xaml.cs
+++ ShootEmUpMaker/ShootEmUpMaker/WelcomeWindow.xaml.cs
@@ -44,7 +44,7 @@ namespace ShootEmUpMaker
 
                 border.Child = text;
                 border.Name = "Game" + i.ToString();
-                border.Background = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+                border.Background = new SolidColorBrush(Color.FromRgb(194, 32, 38));
                 border.Margin = new Thickness(5, 5, 5, 5);
 
                 this.Games.Children.Insert(0, border);
@@ -60,46 +60,12 @@ namespace ShootEmUpMaker
             InitializeComponent();
 
             // Init events
-
+            // export
+            // impport
+            // close (save or cancel dialog)
 
             // Default actions
             LoadCreatedGames();
-
-            //try
-            //{
-            //    int i = 1;
-                //string[] filePaths = Directory.GetFiles(@finalpath, "*.xml");
-                //foreach (string x in filePaths)
-                //{
-                //    var border = new Border();
-                //    var text = new TextBlock();
-                //    text.HorizontalAlignment = HorizontalAlignment.Center;
-                //    text.VerticalAlignment = VerticalAlignment.Center;
-                //    text.FontSize = 36;
-                //    text.Foreground = new SolidColorBrush(Color.FromRgb(255,255,255));
-                //    //text.FontWeight = FontWeight;
-                //    text.Text = x;
-                //    //<TextBlock x:Name="CreateButton" Text="+" HorizontalAlignment="Center"
-                //    //VerticalAlignment="Center" ToolTip="Click here to create a new game" FontSize="36" Foreground="White" FontWeight="Thin" />
-
-                //    border.Child = text;
-                //    border.Name = "Game"  + i.ToString();
-                //    border.Width = 140;
-                //    border.Height = 260;
-                //    border.Background = new SolidColorBrush(Color.FromRgb(0, 111, 111));
-                //    border.Margin = new Thickness(5,5,5,5);
-
-                //    this.Games.Children.Add(border);
-                //    i++;
-                //    MessageBox.Show(x);
-            //    }
-            //}
-            
-            //catch (Exception e)
-            //{
-            //    Console.WriteLine("The process failed: {0}", e.ToString());
-            //}
-
 
             //Enemy Ship
             EnemyShip enemy = new EnemyShip();
@@ -125,14 +91,13 @@ namespace ShootEmUpMaker
             lvl._enemy.Add(enemy);
 
             ShootEmUpGame myGame = new ShootEmUpGame();
-
+            myGame.Name = "Best game ever";
+            myGame._description = "This is my game";
             myGame._author = "Alex";
-            myGame._descrption = "This is my game";
-            myGame._gameName = "Best game ever";
             myGame._orientation = 0;
             myGame._player = player;
-            myGame._level.Add(lvl);
-            myGame._level.Add(lvl);
+            myGame._levels.Add(lvl);
+            myGame._levels.Add(lvl);
 
             //Serialization
             Serialization.ExportGame(myGame);
