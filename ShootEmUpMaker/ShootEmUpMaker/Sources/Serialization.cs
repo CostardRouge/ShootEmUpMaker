7fbaf9f4f09751e00219a4e3dd18c4dfac5242b4
Steeve Pommier <pommier.steeve@gmail.com>
2014-04-24 06:38:44 +0200

    add new attributes to game class

--- ShootEmUpMaker/ShootEmUpMaker/Sources/Serialization.cs
+++ ShootEmUpMaker/ShootEmUpMaker/Sources/Serialization.cs
@@ -14,15 +14,15 @@ namespace ShootEmUpMaker
         {
             //Create game folder
             string docPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
-            Directory.CreateDirectory(docPath + "\\ShootEmUpMaker\\" + myGame.Name);
+            Directory.CreateDirectory(docPath + "\\ShootEmUpMaker\\" + myGame._gameName);
 
             //Create xml file in game folder    
             XmlSerializer xs = new XmlSerializer(typeof(ShootEmUpGame));
             using (StreamWriter wr = new StreamWriter(docPath +
                 "\\ShootEmUpMaker\\" + 
-                myGame.Name + 
+                myGame._gameName + 
                 "\\" +
-                myGame.Name + ".xml"))
+                myGame._gameName + ".xml"))
             {
                 xs.Serialize(wr, myGame);
             }
